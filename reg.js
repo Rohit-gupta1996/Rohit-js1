63d7b5a6a9f476a7bffb860f4e123c8888c57f6e
Rohit-gupta1996 <rohitgpt12slp@gmail.com>
2022-01-21 19:40:36 +0530

    first commit

---
+++ reg.js
@@ -0,0 +1,10 @@
+function sample()
+     {
+var a=document.getElementById("t1").value;
+var b=document.getElementById("t2").value;
+var c=parseInt(a)
+var d=parseInt(b)
+var e=c+d;
+document.getElementById("p1").innerHTML="sum is "+e;
+     }
+   
